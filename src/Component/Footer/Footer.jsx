079ac904748adca5c5ed3028a969544512186bcb
Sagar <sagarjha2004@gmail.com>
2025-01-11 12:26:26 +0530

    updated footer

--- src/Component/Footer/Footer.jsx
+++ src/Component/Footer/Footer.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import logoF from "../../assets/logoF.png";
+import Vector from "../../assets/DSEULogo/Vector.svg";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faPhone,
@@ -19,15 +19,26 @@ const Footer = () => {
       className="bg-white text-black py-8 border-t-4 border-gray-300 shadow-md"
       style={{ fontFamily: "'Inter', sans-serif" }}
     >
-      {/* Created by Hardik Jainwal */}
-      <div className="max-w-6xl mx-auto px-4 flex flex-wrap justify-between items-start">
+      <div className="max-w-7xl mx-auto px-6 flex flex-wrap justify-between items-start space-y-8 md:space-y-0">
+        {/* Logo and Address Section */}
         <div className="w-full md:w-1/3 text-center md:text-left mb-6 md:mb-0">
-          <img
-            src={logoF}
-            alt="DSEU Logo"
-            className="mx-auto md:mx-0 mb-4 w-50 h-20"
-          />
-          <p className="text-sm leading-5">
+          <div className="flex flex-col md:flex-row items-center md:items-start">
+            <img
+              src={Vector}
+              alt="DSEU Logo"
+              className="mb-4 md:mb-0 w-50 h-20"
+            />
+            <div className="ml-4 mt-2 flex flex-col justify-center">
+              
+              <p
+                className="text-lg font-semibold text-center md:text-left"
+                style={{ color: "#005CB9" }}
+              >
+                Delhi Skill and Entrepreneurship University
+              </p>
+            </div>
+          </div>
+          <p className="text-sm leading-5 mt-8">
             <FontAwesomeIcon
               icon={faMapMarkerAlt}
               className="mr-2"
@@ -54,42 +65,36 @@ const Footer = () => {
             />
             helpdesk@dseu.ac.in
             <br />
-            <FontAwesomeIcon
-              icon={faGlobe}
-              className="mr-2"
-              style={{ color: "#005CB9" }}
-            />
-            <a href="https://www.dseu.ac.in" className="text-blue-400">
-              www.dseu.ac.in
-            </a>
+            
           </p>
-          <div className="mt-4 flex justify-center md:justify-start space-x-4">
+          <div className="mt-6 flex justify-center md:justify-start space-x-8">
             <a href="#" aria-label="Twitter">
               <FontAwesomeIcon
                 icon={faTwitter}
-                style={{ color: "#1DA1F2", fontSize: "20px" }}
+                style={{ color: "#1DA1F2", fontSize: "28px" }}
               />
             </a>
             <a href="#" aria-label="LinkedIn">
               <FontAwesomeIcon
                 icon={faLinkedin}
-                style={{ color: "#0077B5", fontSize: "20px" }}
+                style={{ color: "#0077B5", fontSize: "28px" }}
               />
             </a>
             <a href="#" aria-label="Facebook">
               <FontAwesomeIcon
                 icon={faFacebook}
-                style={{ color: "#4267B2", fontSize: "20px" }}
+                style={{ color: "#4267B2", fontSize: "28px" }}
               />
             </a>
           </div>
         </div>
 
+        {/* Quick Links */}
         <div className="w-full md:w-1/3 mb-6 md:mb-0 md:pl-4">
           <h3 className="text-lg font-semibold border-b-2 border-orange-400 inline-block mb-4">
             Quick Links
           </h3>
-          <ul className="space-y-2">
+          <ul className="space-y-3">
             <li>
               <a href="#" className="hover:text-blue-600">
                 Departments
@@ -122,17 +127,18 @@ const Footer = () => {
             </li>
             <li>
               <a href="#" className="hover:text-blue-800">
-                Switch to Dark Mode
+                Commuinication Directory
               </a>
             </li>
           </ul>
         </div>
 
+        {/* Explore */}
         <div className="w-full md:w-1/3">
           <h3 className="text-lg font-semibold border-b-2 border-orange-400 inline-block mb-4">
             Explore
           </h3>
-          <ul className="space-y-2">
+          <ul className="space-y-3">
             <li>
               <a href="#" className="hover:text-blue-600">
                 Rules
@@ -176,8 +182,3 @@ const Footer = () => {
 };
 
 export default Footer;
-
-
-
-
-
